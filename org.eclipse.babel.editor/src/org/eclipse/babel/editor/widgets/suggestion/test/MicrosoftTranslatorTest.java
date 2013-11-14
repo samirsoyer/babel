4739e7d71dd5e20f5ffff80e7fcf35d3fd049c57
samir soyer <samirsoyer@gmail.com>
2013-11-14 14:58:59 +0100

    SuggestionProvider class for Microsoft Translator was created (without implementation) and its test class was implemented. Additionally Java wrapper for Microsoft Translator API was added to project setup

--- org.eclipse.babel.editor/src/org/eclipse/babel/editor/widgets/suggestion/test/MicrosoftTranslatorTest.java
+++ org.eclipse.babel.editor/src/org/eclipse/babel/editor/widgets/suggestion/test/MicrosoftTranslatorTest.java
@@ -2,23 +2,74 @@ package org.eclipse.babel.editor.widgets.suggestion.test;
 
 import static org.junit.Assert.*;
 
+import org.eclipse.babel.editor.widgets.suggestion.model.Suggestion;
+import org.eclipse.babel.editor.widgets.suggestion.provider.MicrosoftTranslatorProvider;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class MicrosoftTranslatorTest {
+	
+	MicrosoftTranslatorProvider mtp;
+	
+	String originalText = "Instances of this class are selectable user " +
+			"interface objects that allow the user to enter and modify text." +
+			" Text controls can be either single or multi-line."; 
+			
+	String targetLanguage = "GERMAN";
+	
+	String translatedText = "Instanzen dieser Klasse sind wählbar " +
+			"Benutzer-Interface-Objekte, mit denen den Benutzer eingeben " +
+			"und Ändern von Text. Text-Steuerelemente können entweder " +
+			"ein- oder mehrzeiligen sein.";
 
 	@Before
 	public void setUp() throws Exception {
+		mtp = new MicrosoftTranslatorProvider();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		mtp = null;
 	}
 
+	/**According to Microsoft Translator API, empty source language string should cause to detect source
+	 * language automatically. If the resulting suggestion doesn't match with already translated suggestion 
+	 * (by Microsoft Translator) test fails.
+	 * */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetSuggestion() {
+		Suggestion actual = mtp.getSuggestion(originalText, targetLanguage);
+		
+		Suggestion expected = new Suggestion(new Image(Display.getCurrent(),"icons/mt16.png"),translatedText);
+		
+		assertNotNull(actual);
+		assertEquals(expected.getText(), actual.getText());
+		assertEquals(expected.getIcon().getImageData(),actual.getIcon().getImageData());
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testGetSuggestionWithNullParameter() {
+		mtp.getSuggestion(null, null);
+		
+//		Suggestion expected = new Suggestion(new Image(Display.getCurrent(),"icons/mt16.png"),translatedText);
+//		
+//		assertNotNull(actual);
+//		assertEquals(expected.getText(), actual.getText());
+//		assertEquals(expected.getIcon().getImageData(),actual.getIcon().getImageData());
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testGetSuggestionShouldThrowIllegalArgumentException() {
+		mtp.getSuggestion("", "");
+		
+//		Suggestion expected = new Suggestion(new Image(Display.getCurrent(),"icons/mt16.png"),translatedText);
+//		
+//		assertNotNull(actual);
+//		assertEquals(expected.getText(), actual.getText());
+//		assertEquals(expected.getIcon().getImageData(),actual.getIcon().getImageData());
 	}
 
 }
