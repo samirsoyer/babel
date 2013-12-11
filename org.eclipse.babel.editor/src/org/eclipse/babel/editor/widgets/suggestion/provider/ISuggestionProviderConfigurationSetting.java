4e1bcb9e366ea107284aa207b50d53cb0e69ee1b
samir soyer <samirsoyer@gmail.com>
2013-12-11 22:36:33 +0100

    Added Javadoc

--- org.eclipse.babel.editor/src/org/eclipse/babel/editor/widgets/suggestion/provider/ISuggestionProviderConfigurationSetting.java
+++ org.eclipse.babel.editor/src/org/eclipse/babel/editor/widgets/suggestion/provider/ISuggestionProviderConfigurationSetting.java
@@ -1,8 +1,24 @@
 package org.eclipse.babel.editor.widgets.suggestion.provider;
 
+/** Interface for suggestion provider
+ * configuration settings. Classes that
+ * implements this interface can represent
+ * the configuration as any type of object,
+ * which depends on the implementation of
+ * {@code updateConfigurationSetting()} method
+ * in {@link ISuggestionProvider}.
+ * @author Samir Soyer
+ *
+ */
 public interface ISuggestionProviderConfigurationSetting {
 	
-	//TODO javadoc
+	
+	/**
+	 * @return configuration setting in any type of object.
+	 * Note: implementation of {@code updateConfigurationSetting()} method
+     * in {@link ISuggestionProvider} should be able to handle the returned
+     * object from this method. 
+	 */
 	Object getConfigurationSetting();
 
 }
--- org.eclipse.babel.editor/src/org/eclipse/babel/editor/widgets/suggestion/provider/StringConfigurationSetting.java
+++ org.eclipse.babel.editor/src/org/eclipse/babel/editor/widgets/suggestion/provider/StringConfigurationSetting.java
@@ -1,24 +1,48 @@
 package org.eclipse.babel.editor.widgets.suggestion.provider;
 
+/**
+ * This class contains configuration setting for
+ * {@link ISuggestionProvider} in string format,
+ * i.e object that contains the configuration setting
+ * is a string. 
+ * @author Samir Soyer
+ *
+ */
 public class StringConfigurationSetting implements
 		ISuggestionProviderConfigurationSetting {
 	
 	private String config;
 	
-	//TODO javadoc
+	/**
+	 * Constructor
+	 * @param config is the string that contains the
+	 * configuration, e.g {@code "/home/xyz/file.xml"}
+	 */
 	public StringConfigurationSetting(String config) {
 		super();
 		this.config = config;
 	}
 
+	/**
+	 * @return configuration as string
+	 */
 	public String getConfig() {
 		return config;
 	}
 
+	/**
+	 * @param config is the string that contains the
+	 * configuration, e.g {@code "/home/xyz/file.xml"
+	 */
 	public void setConfig(String config) {
 		this.config = config;
 	}
 
+	/**
+	 * @see org.eclipse.babel.editor.widgets
+	 * .suggestion.provider
+	 * .ISuggestionProviderConfigurationSetting#getConfigurationSetting()
+	 */
 	@Override
 	public Object getConfigurationSetting() { 
 		return config;
