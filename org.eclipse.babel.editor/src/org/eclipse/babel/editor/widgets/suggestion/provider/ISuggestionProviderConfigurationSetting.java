ed5d44012a7dba8554f8633894fa7241019aed23
samir soyer <samirsoyer@gmail.com>
2014-01-08 00:53:28 +0100

    missing license headers added

--- org.eclipse.babel.editor/src/org/eclipse/babel/editor/widgets/suggestion/provider/ISuggestionProviderConfigurationSetting.java
+++ org.eclipse.babel.editor/src/org/eclipse/babel/editor/widgets/suggestion/provider/ISuggestionProviderConfigurationSetting.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Samir Soyer.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Samir Soyer - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.babel.editor.widgets.suggestion.provider;
 
 /** Interface for suggestion provider
